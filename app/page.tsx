043ce18d8aa03f6174d2679c7b21a59562c3cc25
Vishnu Vinayan <vishnu.vinayan@nysaa.com>
2024-03-13 19:36:34 +0400

    added dynamic button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello World</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10">something</Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
 }
