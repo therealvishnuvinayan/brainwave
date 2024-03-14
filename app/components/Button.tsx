9ab226148cd6b5d0170625397f282d13483d88f9
Vishnu Vinayan <vishnu.vinayan@nysaa.com>
2024-03-14 19:42:19 +0400

    Mobile header added

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -25,7 +25,7 @@ const Button = ({
   const spanClasses = "relative z-10";
 
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import Image from "next/image";
 import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
 
 const Header = () => {
   const [hash, setHash] = useState("");
+  const [openNavigation, setOpenNavigation] = useState(false);
   const { src, width, height } = brainwave;
 
   useEffect(() => {
@@ -21,19 +25,44 @@ const Header = () => {
     };
   }, []);
 
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
+  };
+
+  const handleCloseNavigation = () => {
+    if(!openNavigation) return;
+    enablePageScroll();
+    setOpenNavigation(false);
+  };
+
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a href="#hero" className="block w-[12rem] xl:mr-8">
           <Image src={src} alt="brainwave-logo" width={width} height={height} />
         </a>
-        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparant">
+        <nav
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparant`}
+        >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
                 href={item.url}
                 id={item.id}
                 key={item.id}
+                onClick={handleCloseNavigation}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
@@ -44,7 +73,9 @@ const Header = () => {
               </a>
             ))}
           </div>
+          <HamburgerMenu />
         </nav>
+
         <a
           href="##signup"
           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
@@ -53,6 +84,14 @@ const Header = () => {
         </a>
         <Button className="hidden lg:flex" href="#login">
           Sign in
+        </Button>
+
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
     </div>
