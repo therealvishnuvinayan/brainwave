38b03588b0eccb64e0993881c018cbecec3b9154
Vishnu Vinayan <vishnu.vinayan@nysaa.com>
2024-03-16 21:16:53 +0400

    deleting dev files

---
+++ app/components/design/dist/Roadmap.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.Gradient = void 0;
+var assets_1 = require("../../assets");
+exports.Gradient = function () {
+    return (React.createElement("div", { className: "absolute top-[18.25rem] -left-[30.375rem] w-[56.625rem] opacity-60 mix-blend-color-dodge pointer-events-none" },
+        React.createElement("div", { className: "absolute top-1/2 left-1/2 w-[58.85rem] h-[58.85rem] -translate-x-3/4 -translate-y-1/2" },
+            React.createElement("img", { className: "w-full", src: assets_1.gradient, width: 942, height: 942, alt: "Gradient" }))));
+};
