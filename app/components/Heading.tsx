d916cab5b90fbc7c21b4ddb3cdae412e7f82cb97
Vishnu Vinayan <vishnu.vinayan@nysaa.com>
2024-03-20 19:06:51 +0400

    Added services

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -1,12 +1,14 @@
 interface HeadingProps {
   className: string;
   title: string;
+  text: string
 }
 
-const Heading: React.FC<HeadingProps> = ({ className, title }) => {
+const Heading: React.FC<HeadingProps> = ({ className, title, text }) => {
   return (
     <div className={`${className} max-w-50rem mx-auto mb-12 lg:mb-20`}>
       {title && <h2 className="h2">{title}</h2>}
+      {text && <p className="body-2 mt-4 tex-n-4">{text}</p>}
     </div>
   );
 };
--- app/components/design/Services.tsx
+++ app/components/design/Services.tsx
@@ -16,7 +16,7 @@ export const Gradient = () => {
   );
 };
 
-export const PhotChatMessage = () => {
+export const PhotoChatMessage = () => {
   return (
     <div className="absolute top-8 right-8 max-w-[17.5rem] py-6 px-8 bg-black rounded-t-xl rounded-bl-xl font-code text-base lg:top-16 lg:right-[8.75rem] lg:max-w-[17.5rem]">
       Hey Brainwave, enhance this photo
@@ -30,7 +30,7 @@ export const VideoChatMessage = () => {
     <div className="absolute top-8 left-[3.125rem] w-full max-w-[14rem] pt-2.5 pr-2.5 pb-7 pl-5 bg-n-6 rounded-t-xl rounded-br-xl font-code text-base md:max-w-[17.5rem]">
       Video generated!
       <div className="absolute left-5 -bottom-[1.125rem] flex items-center justify-center w-[2.25rem] h-[2.25rem] bg-color-1 rounded-[0.75rem]">
-        <img
+        <Image
           src={brainwaveWhiteSymbol}
           width={26}
           height={26}
@@ -51,7 +51,7 @@ export const VideoChatMessage = () => {
 export const VideoBar = () => {
   return (
     <div className="absolute left-0 bottom-0 w-full flex items-center p-6">
-      <img
+      <Image
         src={play}
         width={24}
         height={24}
